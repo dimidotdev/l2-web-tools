852a7f04f041e2c61dfb4a329d065c7215c85a03
dimidotdev <worcx0@proton.me>
2025-02-10 19:30:46 -0300

    NAD page advertising

--- src/app/nads/[ticketId]/page.tsx
+++ src/app/nads/[ticketId]/page.tsx
@@ -9,11 +9,16 @@ export default async function NadDetail(props: {params: Promise<{ticketId: strin
   if (!nad.ticketId) return <NotFound />;
 
   return (
-    <div className="container mx-auto p-8 bg-white shadow-lg rounded-lg flex flex-col items-center w-1/2">
-      <h1 className="text-4xl font-bold mb-4">{nad.ticketId}</h1>
-      <p className="text-2xl font-bold mb-1">{nad.customerName}</p>
-      <p className="text-2xl font-style: italic mb-1">{nad.targetUrl}</p>
-      <p className="text-1xl mb-1">{nad.creationDate}</p>
-    </div>
+    <>
+      <div className="container mx-auto p-8 bg-white shadow-lg rounded-lg flex flex-col items-center w-1/2">
+        <h1 className="text-4xl font-bold mb-4">{nad.ticketId}</h1>
+        <p className="text-2xl font-bold mb-1">{nad.customerName}</p>
+        <p className="text-2xl font-style: italic mb-1">{nad.targetUrl}</p>
+        <p className="text-1xl mb-1">{nad.creationDate}</p>
+      </div>
+      <div>
+        <h2>Feature currently in development</h2>
+      </div>
+    </>
   );
 }
