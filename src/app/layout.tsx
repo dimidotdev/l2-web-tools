ed38d2e3c61936859a84bf534e77612c106f9661
dimidotdev <worcx0@proton.me>
2025-02-11 23:08:47 -0300

    fix: metadata

--- src/app/layout.tsx
+++ src/app/layout.tsx
@@ -6,6 +6,11 @@ import Navbar from './components/Navbar';
 
 const inter = Inter({ subsets: ['latin'] });
 
+export const metadata = {
+  title: 'L2 Tools',
+  description: 'NAD documentation and tools',
+};
+
 export default function RootLayout({
   children,
 }: {
